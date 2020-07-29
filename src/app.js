7ccd98ece4151a83286024869a3792476f8514f4
Gabriel <knuckles4x@gmail.com>
2020-07-29 18:59:29 -0300

    heroku deploy settings

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "public")));
